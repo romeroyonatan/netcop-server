85425bf4d7791563bb3e834dd80d9aabcab078f1
Yonatan Romero <yromero@openmailbox.org>
2016-07-26 13:56:09 -0300

    Intento carga lista de protocolo y subredes

--- netcop/clases/forms.py
+++ netcop/clases/forms.py
@@ -10,3 +10,101 @@ class ClaseForm(forms.ModelForm):
     class Meta:
         model = models.ClaseTrafico
         fields = '__all__'
+
+    subredes_outside = forms.CharField(
+        widget=forms.Textarea,
+        required=False,
+        help_text="""Lista de redes en Internet (separadas por nueva linea) con
+        el formato <strong>direccion/prefijo</strong> donde la direccion es el
+        identificador de red y el prefijo es la cantidad de bits que contiene
+        la máscara de subred. Si no se ingresa un prefijo, se entiende que se
+        trata de una dirección de host.""",
+    )
+    puertos_outside = forms.CharField(
+        widget=forms.Textarea,
+        required=False,
+        help_text="""Lista de puertos en Internet (separados por nueva linea)
+        con el formato <strong>numero/protocolo</strong> donde el numero es el
+        identificador del puerto y el protocolo puede ser 'tcp' o 'udp'. Si no
+        se especifica protocolo, se asume que el puerto puede pertenecer a
+        cualquiera de ellos.""",
+    )
+    subredes_inside = forms.CharField(
+        widget=forms.Textarea,
+        required=False,
+        help_text="""Lista de redes en la red local (separadas por nueva linea)
+        con el formato <strong>direccion/prefijo</strong> donde la direccion es
+        el identificador de red y el prefijo es la cantidad de bits que
+        contiene la máscara de subred. Si no se ingresa un prefijo, se entiende
+        que se trata de una dirección de host.""",
+    )
+    puertos_inside = forms.CharField(
+        widget=forms.Textarea,
+        required=False,
+        help_text="""Lista de puertos en la red local (separados por nueva
+        linea) con el formato <strong>numero/protocolo</strong> donde el numero
+        es el identificador del puerto y el protocolo puede ser 'tcp' o 'udp'.
+        Si no se especifica protocolo, se asume que el puerto puede pertenecer
+        a cualquiera de ellos.""",
+    )
+
+    def save(self, *args, **kwargs):
+        clase = super().save(*args, **kwargs)
+        self.actualizar_colecciones(clase, self.cleaned_data)
+        return clase
+
+    def actualizar_colecciones(self, clase, nueva):
+        '''
+        Actualiza las listas de subredes y puertos de la clase de trafico.
+        '''
+        redes = (('subredes_outside', models.OUTSIDE),
+                 ('subredes_inside', models.INSIDE))
+        puertos = (('puertos_outside', models.OUTSIDE),
+                   ('puertos_inside', models.INSIDE))
+
+        # borro antiguas relaciones
+        models.ClaseCIDR.objects.filter(clase=clase).delete()
+        models.ClasePuerto.objects.filter(clase=clase).delete()
+
+        # creo las nuevas relaciones
+        for lista, grupo in redes:
+            for item in nueva.get(lista, "").split("\n"):
+                if item:
+                    (direccion, prefijo) = item.split('/')
+                    # TODO validar
+                    cidr = models.CIDR.objects.get_or_create(
+                        direccion=direccion,
+                        prefijo=prefijo
+                    )[0]
+                    models.ClaseCIDR.objects.create(clase=clase,
+                                                    cidr=cidr,
+                                                    grupo=grupo)
+
+        for lista, grupo in puertos:
+            for item in nueva.get(lista, "").split("\n"):
+                if item:
+                    (numero, proto) = item.split('/')
+                    # TODO validar
+                    protocolo = self.protocolo(proto)
+                    puerto = models.Puerto.objects.get_or_create(
+                        numero=numero,
+                        protocolo=protocolo
+                    )[0]
+                    models.ClasePuerto.objects.create(clase=clase,
+                                                      puerto=puerto,
+                                                      grupo=grupo)
+
+    def protocolo(self, string):
+        '''
+        Obtiene el numero de protocolo en base a una cadena de caracteres.
+        Retornos
+        ---------------
+          * 6 - TCP | tcp
+          * 17 - UDP | udp
+          * 0 en cualquier otro caso.
+        '''
+        if string.lower() == 'tcp':
+            return 6
+        elif string.lower() == 'udp':
+            return 17
+        return 0
--- netcop/clases/models.py
+++ netcop/clases/models.py
@@ -13,20 +13,34 @@ class ClaseTrafico(models.Model):
     descripcion = models.CharField(max_length=160, null=False, default="")
     activa = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.nombre
+
 
 class CIDR(models.Model):
     direccion = models.GenericIPAddressField(protocol='IPv4')
     prefijo = models.PositiveSmallIntegerField(default=32)
+
+    def __str__(self):
+        return "%s/%d" % (self.direccion, self.prefijo)
 
 
 class Puerto(models.Model):
     numero = models.PositiveIntegerField()
     protocolo = models.PositiveSmallIntegerField(default=0)
 
+    def __str__(self):
+        protocolo = ''
+        if self.protocolo == 6:
+            protocolo = 'tcp'
+        elif self.protocolo == 17:
+            protocolo = 'udp'
+        return "%d/%s" % (self.numero, protocolo)
+
 
 class ClaseCIDR(models.Model):
-    clase = models.ForeignKey(ClaseTrafico)
-    cidr = models.ForeignKey(CIDR)
+    clase = models.ForeignKey(ClaseTrafico, related_name="redes")
+    cidr = models.ForeignKey(CIDR, related_name="clases")
     grupo = models.CharField(max_length=1, choices=grupo_choices)
 
 
@@ -35,6 +49,6 @@ class ClasePuerto(models.Model):
         (INSIDE, "En la red local"),
         (OUTSIDE, "En Internet"),
     )
-    clase = models.ForeignKey(ClaseTrafico)
-    puerto = models.ForeignKey(Puerto)
+    clase = models.ForeignKey(ClaseTrafico, related_name="puertos")
+    puerto = models.ForeignKey(Puerto, related_name="clases")
     grupo = models.CharField(max_length=1, choices=grupo_choices)
