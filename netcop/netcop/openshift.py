7e7ebc1615dc4ed17b1c0d0519142248e2bc43b7
Yonatan Romero <yromero@openmailbox.org>
2016-07-28 11:23:16 -0300

    Desactivado modo debug en openshoft

--- netcop/netcop/openshift.py
+++ netcop/netcop/openshift.py
@@ -12,11 +12,18 @@ BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
 # directorio de logs
 LOG_DIR = os.environ.get('OPENSHIFT_LOG_DIR', '')
 
+# dominios en los que el sitio funciona. sirve para prevenir ataques
+ALLOWED_HOSTS = ['.rhcloud.com', '.netcop.ftp.sh']
+DEBUG=False
+
 POSTGRE_USERNAME = os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME', '')
 POSTGRE_PASSWORD = os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD', '')
 POSTGRE_HOST = os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST', '')
 POSTGRE_PORT = os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT', '')
 POSTGRE_DB = os.environ.get('PGDATABASE', '')
+
+SECRET_KEY = os.environ.get('OPENSHIFT_SECRET_TOKEN', '')
+
 
 # base de datos productiva
 DATABASES = {
@@ -32,6 +39,6 @@ DATABASES = {
 
 
 # lugar fisico donde se copiaran los archivos estaticos
-STATIC_ROOT = os.path.join(REPO_DIR, 'wsgi/static')
+STATIC_ROOT = os.path.join(REPO_DIR, 'wsgi', 'static')
 # lugar fisico donde se copiaran los archivos que suban los usuarios
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
